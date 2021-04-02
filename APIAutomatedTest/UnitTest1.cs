9b5587b1d1003aa2625adddcffa1014f070437a0
jpantelic <jecap86@gmail.com>
2021-04-02 16:03:13 +0200

    first post

--- APIAutomatedTest/UnitTest1.cs
+++ APIAutomatedTest/UnitTest1.cs
@@ -1,5 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using RestSharp;
+using RestSharp.Authenticators;
+using System.Net;
 
 namespace APIAutomatedTest
 {
@@ -18,6 +20,35 @@ namespace APIAutomatedTest
             string response = restResponse.Content;
 
             System.Console.WriteLine(response);
+
+            Assert.AreEqual(restResponse.StatusCode, HttpStatusCode.OK, "Get request status is not ok");
+        }
+
+        [TestMethod]
+        public void firstPost()
+        {
+                           
+            RestClient restClient = new RestClient("https://postman-echo.com/post");
+            RestRequest restRequest = new RestRequest(Method.POST);
+            restRequest.AddParameter("application/json", "C://People.json", ParameterType.RequestBody);
+            IRestResponse restResponse = restClient.Execute(restRequest);
+
+            System.Console.WriteLine("Post response" + restResponse.Content);
+
+           
+        }
+
+        [TestMethod]
+        public void firstAuthenticators()
+        {
+            RestClient restClient = new RestClient("https://postman-echo.com/basic-auth");
+            restClient.Authenticator = new HttpBasicAuthenticator("postman", "password");
+
+            RestRequest restRequest = new RestRequest("", Method.GET);
+            IRestResponse restResponse = restClient.Execute(restRequest);
+
+            Assert.AreEqual(restResponse.StatusCode, HttpStatusCode.OK, "Get request status, or username/password is/are not ok");
+
         }
     }
 }
